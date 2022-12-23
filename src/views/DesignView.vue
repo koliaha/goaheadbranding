9843805f4f920fce9983a7c45b2e6a88f016fa43
Nikolaychik <nikolay@gomobile.ru>
2022-12-23 21:44:22 +0300

    commit logo

--- src/views/DesignView.vue
+++ src/views/DesignView.vue
@@ -1,8 +1,20 @@
 <template>
-  <section class="brand">
+  <section class="design-first">
     <DesignHeader />
+  </section>
+  <section class="design-second">
     <div class="container">
-      asd
+      <DesignLogo/>
+    </div>
+  </section>
+  <section class="design-third">
+    <div class="container">
+      3
+    </div>
+  </section>
+  <section class="design-forth">
+    <div class="container">
+      4
     </div>
   </section>
 </template>
@@ -10,11 +22,12 @@
 <script>
 // @ is an alias to /src
 import DesignHeader from '@/components/DesignHeader.vue'
+import DesignLogo from '@/components/design/DesignLogo.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    DesignHeader
+    DesignHeader,DesignLogo
   }
 }
 </script>
