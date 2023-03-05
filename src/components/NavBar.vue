70b52d0301f769bc563686d8741dd596df794da3
Nikolaychik <nikolay@gomobile.ru>
2023-03-05 21:14:36 +0300

    test

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,22 @@
 <template>
-    <nav class="nav">
-        <div class="container">
-            <div class="nav-list">
-                <router-link class="nav-item" exact-active-class="active" to="/">Бренд</router-link>
-                <router-link class="nav-item" exact-active-class="active" to="/content">Контент</router-link>
-                <router-link class="nav-item" exact-active-class="active" to="/design">Дизайн</router-link>
-            </div>
-        </div>
-    </nav>
+  <nav class="nav">
+    <div class="container">
+      <div class="nav-list">
+        <router-link class="nav-item" exact-active-class="active" to="/"
+          >Бренд</router-link
+        >
+        <router-link class="nav-item" exact-active-class="active" to="/content"
+          >Контент</router-link
+        >
+        <router-link class="nav-item" exact-active-class="active" to="/design"
+          >Дизайн</router-link
+        >
+      </div>
+    </div>
+  </nav>
 </template>
 <script>
-
 export default {
-    name: 'navBar',
-
-}
+  name: "navBar",
+};
 </script>
